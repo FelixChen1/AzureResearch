b5d0a22b374b05f1af62e5a8b83873f78b5fa853
FelixChen1 <felix.chen.zhuhai@quest.com>
2018-04-02 18:17:44 +0800

    Init a cosmos demo console

---
+++ CosmosDBDemo/DemoConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Azure.Documents.Client;
+using Newtonsoft.Json;
+
+namespace DemoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            const string endpointUrl = "https://localhost:8081";
+            const string primaryKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+
+            DocumentClient client = new DocumentClient(new Uri(endpointUrl), primaryKey);
+        }
+    }
+}
